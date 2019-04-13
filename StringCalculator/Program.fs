459d5e43283c3ad2a622f04c801e6ea27166140a
WaedSaleh <waedjabaren@gmail.com>
2019-04-13 17:29:31 -0400

    Handle multiple delimiters

--- StringCalculator/Program.fs
+++ StringCalculator/Program.fs
@@ -3,27 +3,43 @@
 open System
 open Microsoft.FSharp.Core
 open System.Text.RegularExpressions
+open System.Linq
 
 module StringCalculator = 
 // Function that checks if an string is number
  let isDigit d = fst (System.Int32.TryParse(d))
+ // get numbers partition 
+ let getNumbers  input =
+   let m = Regex.Match(input,@"^(//.*\n)(.*)") 
+   if (m.Success) then   m.Groups.[2].Value else input 
+// function to check the pattern and return first group
+ let (|FirstRegexGroup|_|) pattern input =
+   let m = Regex.Match(input,pattern) 
+   if (m.Success) then Some m.Groups.[1].Value else None  
+
 // get delimiter based on a pattern
- let getDelimiter (s,pattern:string) =
-   let m = Regex.Match(s, pattern) 
-   if (m.Success) then 
-   let d = Regex.Match(s,pattern).Groups.[1].Value
-   d.Substring(2,(d.LastIndexOf("\n")-2)) else ","  
+ let getDelimiter s : string []=
+     match s with
+    | FirstRegexGroup  "^(//\[.*\]\n)(.*)" host -> 
+          host.Substring(2,(host.LastIndexOf("\n")-2)).Replace(']',' ').Replace('[',' ' ).Split(" ").Where(fun n-> n <> "").ToArray()
+                     
+    | FirstRegexGroup "^(//.*\n)(.*)" host -> 
+             host.Substring(2,(host.LastIndexOf("\n")-2)).Split("")
+    | _ -> [|","; "\n"|]  
     
 // add numbers function that calculate sume of numbers in string 
  let addNumbers (input:string):int =
- try
+  try
 // check if input is empty
   if(String.IsNullOrEmpty(input)) then 0 else
 // if input is followed by two chacter
-   if(Regex.IsMatch(input, "\d[^a-z0-9-][^a-z0-9-]")) then -1 else 
+   if(Regex.IsMatch(input, "\d,\n")) then -1 else 
+// get delimiter based on a pattern 
+  let d = getDelimiter (input)
+// get numbers part
+  let  values = getNumbers input
 // split input by delimiter 
-  let d = getDelimiter (input, "^(//.*\n)(.*)")
-  let numbers = input.Split([|d; "\n"|], StringSplitOptions.None)
+  let numbers = values.Split(d, StringSplitOptions.None)
  // handle negative numbers
   if( Array.exists (fun elem -> isDigit elem && System.Int32.Parse elem < 0) numbers)  
   then failwith("Negatives not allowed: " + String.Join(", ",  numbers |> Seq.filter (fun x ->   isDigit x && System.Int32.Parse x < 0))) 
@@ -34,11 +50,11 @@ module StringCalculator =
   (sum)
    with
     | :? System.Exception as ex -> 
-          Console.Write( ex.Message);-1
+          Console.Write( ex.Message); -1
 [<EntryPoint>]
 let main arge = 
 // Test 1 : "1"
-System.Console.WriteLine(String.Concat("Test 1: (1) ", StringCalculator.addNumbers ("1")))
+System.Console.WriteLine(String.Concat("Test 1: (1) ",  StringCalculator.addNumbers ("1")))
 // Test 2 : ""
 System.Console.WriteLine(String.Concat(@"Test 2: ("") ",StringCalculator.addNumbers ("")))
 // Test 3 : test case when send ; delimiter at first line 
@@ -47,11 +63,18 @@ System.Console.WriteLine(String.Concat(@"Test 3: with ; as delimiter  (//;\n1;2;
 System.Console.WriteLine(String.Concat(@"Test 4: (1;2;4) ",StringCalculator.addNumbers ("1;2;4")))
 // Test 5 : test case when send,  delimiter at first line 
 System.Console.WriteLine(String.Concat(@"Test 5: (//,\n1,2,9) ",StringCalculator.addNumbers ("//,\n1,2,9")))
-// Test 6 : test case when send \n  delimiter at first line 
-System.Console.WriteLine(String.Concat(@"Test 6: (//\n\n1\n-2\n-9) ",StringCalculator.addNumbers ("//\n\n1\n-2\n-9")))
 // Test 7 : test case when send delimiter is not in  first line 
 System.Console.WriteLine(String.Concat(@"Test 7: (1,2\n3) ",StringCalculator.addNumbers ("1,2\n3")))
+// Test 8 : test case when send \n  delimiter at first line 
+System.Console.WriteLine(String.Concat(@"Test 8: (//[***]\n1***2***9) ",StringCalculator.addNumbers ("//[***]\n1***2***9")))
 // invlaid input return -1
-System.Console.WriteLine(String.Concat(@"Test 8: invalid input return -1 (1,\n) ",StringCalculator.addNumbers ("1,\n")))
-System.Console.WriteLine(String.Concat(@"Test 9: Test Case when a number more than 1001 (1,1000,1001,500) ",StringCalculator.addNumbers ("1,1000,1001,500")))
+System.Console.WriteLine(String.Concat(@"Test 9: invalid input return -1 (1,\n) ",StringCalculator.addNumbers ("1,\n")))
+// ignore numbers bigger than 1000
+System.Console.WriteLine(String.Concat(@"Test 10: Test Case when a number bigger than 1001 (1,1000,1001,500) ",StringCalculator.addNumbers ("1,1000,1001,500")))
+//negative numbers
+System.Console.WriteLine(String.Concat(@" //Test 11: Calling Add with a negative number (1,-9,-8) ",StringCalculator.addNumbers ("1,-9,-8")))
+// Test 8 : test case when send \n  delimiter at first line 
+System.Console.WriteLine(String.Concat(@"Test 12 multiple delimiter: (//[*][%]\n1*2%3) ",StringCalculator.addNumbers ("//[*][%]\n1*2%3")))
+// Test 8 : test case when send \n  delimiter at first line 
+System.Console.WriteLine(String.Concat(@"Test 12 multiple delimiter: (//[***][%]\n1***2%3) ",StringCalculator.addNumbers ("//[***][%]\n1***2%3")))
 0
